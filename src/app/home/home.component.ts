672238b286304ade0ac22ba3f27baf8841b23d36
ubbn <ubbn@live.com>
2017-02-07 20:02:28 +0100

    Completed data binding on form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { Employee } from '../models/employee.model';
 
 @Component({
   selector: 'home',
@@ -7,4 +8,12 @@ import {Component} from '@angular/core';
 })
 export class HomeComponent {
   languages = ['English', 'Spanish', 'Swedish', 'Other'];
+  model: Employee = new Employee('Bat', 'Dorj', true, '1099', 'Swedish');
+
+  toUpperCase(value: string): void {
+    if (value.length > 0)
+      this.model.firstName = value.charAt(0).toUpperCase() + value.slice(1);
+    else
+      this.model.firstName = value;
+  }
 }
